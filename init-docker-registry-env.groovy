b07be0fb83448835ffac30a0fa3ef569447c775a
James Strachan <james.strachan@gmail.com>
2018-06-08 15:58:55 +0100

    add better null handling
    thanks JamesN

--- init-docker-registry-env.groovy
+++ init-docker-registry-env.groovy
@@ -1,11 +1,18 @@
 import hudson.slaves.EnvironmentVariablesNodeProperty
 import hudson.EnvVars
-import jenkins.model.*;
+import jenkins.model.Jenkins
 
 def KEY = "DOCKER_REGISTRY"
 def HOST_ENV = "JENKINS_X_DOCKER_REGISTRY_SERVICE_HOST"
 def PORT_ENV = "JENKINS_X_DOCKER_REGISTRY_SERVICE_PORT"
-def globals = Jenkins.instance.getGlobalNodeProperties().get(EnvironmentVariablesNodeProperty.class)
+
+
+def globals = Jenkins.get().getGlobalNodeProperties().get(EnvironmentVariablesNodeProperty.class)
+if (globals == null) {
+  globals = new EnvironmentVariablesNodeProperty()
+  Jenkins.get().getGlobalNodeProperties().add(globals)
+}
+
 def envVars = globals.getEnvVars();
 if (envVars.get(KEY) == null || envVars.get(KEY).length() == 0) {
   // is there a system environment
